4a40efc0765c0a991973b5fccb3e23f7ac58cb8d
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-09-09 13:53:33 +0300

    Initial AOSP jellybean commit

--- gapps.mk
+++ gapps.mk
@@ -3,26 +3,28 @@
 #
 PRODUCT_COPY_FILES += \
     vendor/google/gapps/system/app/ChromeBookmarksSyncAdapter.apk:$(PRODUCT_OUT)/system/app/ChromeBookmarksSyncAdapter.apk \
-    vendor/google/gapps/system/app/GalleryGoogle.apk:$(PRODUCT_OUT)/system/app/GalleryGoogle.apk \
     vendor/google/gapps/system/app/GenieWidget.apk:$(PRODUCT_OUT)/system/app/GenieWidget.apk \
     vendor/google/gapps/system/app/Gmail.apk:$(PRODUCT_OUT)/system/app/Gmail.apk \
+    vendor/google/gapps/system/app/GmsCore.apk:$(PRODUCT_OUT)/system/app/GmsCore.apk \
     vendor/google/gapps/system/app/GoogleBackupTransport.apk:$(PRODUCT_OUT)/system/app/GoogleBackupTransport.apk \
     vendor/google/gapps/system/app/GoogleCalendarSyncAdapter.apk:$(PRODUCT_OUT)/system/app/GoogleCalendarSyncAdapter.apk \
     vendor/google/gapps/system/app/GoogleContactsSyncAdapter.apk:$(PRODUCT_OUT)/system/app/GoogleContactsSyncAdapter.apk \
+    vendor/google/gapps/system/app/GoogleEars.apk:$(PRODUCT_OUT)/system/app/GoogleEars.apk \
     vendor/google/gapps/system/app/GoogleFeedback.apk:$(PRODUCT_OUT)/system/app/GoogleFeedback.apk \
     vendor/google/gapps/system/app/GoogleLoginService.apk:$(PRODUCT_OUT)/system/app/GoogleLoginService.apk \
     vendor/google/gapps/system/app/GooglePartnerSetup.apk:$(PRODUCT_OUT)/system/app/GooglePartnerSetup.apk \
-    vendor/google/gapps/system/app/GoogleQuickSearchBox.apk:$(PRODUCT_OUT)/system/app/GoogleQuickSearchBox.apk \
     vendor/google/gapps/system/app/GoogleServicesFramework.apk:$(PRODUCT_OUT)/system/app/GoogleServicesFramework.apk \
     vendor/google/gapps/system/app/GoogleTTS.apk:$(PRODUCT_OUT)/system/app/GoogleTTS.apk \
-    vendor/google/gapps/system/app/MarketUpdater.apk:$(PRODUCT_OUT)/system/app/MarketUpdater.apk \
+    vendor/google/gapps/system/app/LatinImeDictionaryPack.apk:$(PRODUCT_OUT)/system/app/LatinImeDictionaryPack.apk \
     vendor/google/gapps/system/app/MediaUploader.apk:$(PRODUCT_OUT)/system/app/MediaUploader.apk \
     vendor/google/gapps/system/app/NetworkLocation.apk:$(PRODUCT_OUT)/system/app/NetworkLocation.apk \
     vendor/google/gapps/system/app/OneTimeInitializer.apk:$(PRODUCT_OUT)/system/app/OneTimeInitializer.apk \
+    vendor/google/gapps/system/app/Phonesky.apk:$(PRODUCT_OUT)/system/app/Phonesky.apk \
+    vendor/google/gapps/system/app/QuickSearchBox.apk:$(PRODUCT_OUT)/system/app/QuickSearchBox.apk \
     vendor/google/gapps/system/app/SetupWizard.apk:$(PRODUCT_OUT)/system/app/SetupWizard.apk \
     vendor/google/gapps/system/app/Talk.apk:$(PRODUCT_OUT)/system/app/Talk.apk \
-    vendor/google/gapps/system/app/Vending.apk:$(PRODUCT_OUT)/system/app/Vending.apk \
-    vendor/google/gapps/system/app/VoiceSearch.apk:$(PRODUCT_OUT)/system/app/VoiceSearch.apk
+    vendor/google/gapps/system/app/Talkback.apk:$(PRODUCT_OUT)/system/app/Talkback.apk \
+    vendor/google/gapps/system/app/VoiceSearchStub.apk:$(PRODUCT_OUT)/system/app/VoiceSearchStub.apk
 
 PRODUCT_COPY_FILES += \
     vendor/google/gapps/system/etc/permissions/com.google.android.maps.xml:$(PRODUCT_OUT)/system/etc/permissions/com.google.android.maps.xml \
@@ -39,6 +41,8 @@ PRODUCT_COPY_FILES += \
     vendor/google/gapps/system/lib/libflint_engine_jni_api.so:$(PRODUCT_OUT)/system/lib/libflint_engine_jni_api.so \
     vendor/google/gapps/system/lib/libfrsdk.so:$(PRODUCT_OUT)/system/lib/libfrsdk.so \
     vendor/google/gapps/system/lib/libgcomm_jni.so:$(PRODUCT_OUT)/system/lib/libgcomm_jni.so \
+    vendor/google/gapps/system/lib/libgoogle_recognizer_jni.so:$(PRODUCT_OUT)/system/lib/libgoogle_recognizer_jni.so \
     vendor/google/gapps/system/lib/libpicowrapper.so:$(PRODUCT_OUT)/system/lib/libpicowrapper.so \
     vendor/google/gapps/system/lib/libspeexwrapper.so:$(PRODUCT_OUT)/system/lib/libspeexwrapper.so \
-    vendor/google/gapps/system/lib/libvoicesearch.so:$(PRODUCT_OUT)/system/lib/libvoicesearch.so
+    vendor/google/gapps/system/lib/libvoicesearch.so:$(PRODUCT_OUT)/system/lib/libvoicesearch.so \
+    vendor/google/gapps/system/lib/libvorbisencoder.so:$(PRODUCT_OUT)/system/lib/libvorbisencoder.so
